a0f674a3522ffa9f56eaf0518cfc13bb50666cce
rollingn27 <arollingstone27@gmail.com>
2024-02-13 00:57:41 +0900

    etc: 서버 연결 세팅

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import apiService from './api/apiService'
 
 import App from './App.vue'
 import router from './router'
@@ -11,4 +12,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.config.globalProperties.$axios = apiService.axiosInstance
+app.config.globalProperties.$apiService = apiService
 app.mount('#app')
