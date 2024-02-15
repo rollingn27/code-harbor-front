67721a684eaa2ec6281652b3de772a31fb68cbb8
rollingn27 <arollingstone27@gmail.com>
2024-02-15 20:14:15 +0900

    feat: 전역 라우트 가드 추가
    1. 미 로그인시 로그인 페이지로 이동
    2. 로그인 router 추가
    3. 회원가입 router 추가
    4. 안쓰는 코드 정리

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth.store'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,32 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requireAuth: true }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/signIn',
+      name: 'signIn',
+      component: () => import('../views/SignIn.vue')
+    },
+    {
+      path: '/signUp',
+      name: 'signUp',
+      component: () => import('../views/SignUp.vue')
     }
   ]
 })
 
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore()
+  if (to.matched.some((routeInfo) => routeInfo.meta.requireAuth)) {
+    if (!auth.logined) {
+      next({ path: '/signIn' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
 export default router
