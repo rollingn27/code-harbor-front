39feeb3373579c6d0e904cb2c9ccd9bbc33d56cb
rollingn27 <arollingstone27@gmail.com>
2024-02-16 23:19:41 +0900

    feat: 공통 헤더 생성

---
+++ src/components/common/CodeHarborHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="code-header">
+    <div class="header-left">
+      <img src="@/assets/logoCodeHarbor.png" alt="code harbor" />
+      <div class="code-harbor">Code Harbor <span>with algorithm</span></div>
+    </div>
+    <div class="header-right">
+      <div class="user-info"><span>Rolling</span>님 안녕하세요.</div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.code-header {
+  width: 100%;
+  height: 90px;
+
+  display: flex;
+  padding: 0 2rem;
+  position: relative;
+
+  .header-left,
+  .header-right {
+    width: 50%;
+    overflow: hidden;
+    color: #808080;
+  }
+  .header-left {
+    display: flex;
+    .code-harbor {
+      font-size: 3rem;
+
+      font-weight: bold;
+      margin: auto 0 0 0;
+      span {
+        font-size: 1.25rem;
+      }
+    }
+  }
+  .header-right {
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    font-size: 1.25rem;
+    span {
+      font-size: 2rem;
+      font-style: italic;
+    }
+  }
+}
+.code-header::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 2%;
+  width: 96%;
+  border-bottom: 1px solid #808080;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import LoadingBar from '@/common/LoadingBar.vue'
+import LoadingBar from '@/components/common/LoadingBar.vue'
+import CodeHarborHeader from '@/components/common/CodeHarborHeader.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.component('loading-bar', LoadingBar)
+app.component('code-harbor-header', CodeHarborHeader)
 
 app.mount('#app')
