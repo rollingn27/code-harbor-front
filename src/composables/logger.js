634ed2dd3386d5de2f3fb605be6d88fdb95808e9
rollingn27 <arollingstone27@gmail.com>
2024-02-19 20:48:53 +0900

    fix: 전역 로그에 caller 추가

--- src/composables/logger.js
+++ src/composables/logger.js
@@ -1,12 +1,14 @@
 const useLogger = () => {
   const log = (...messages) => {
     if (process.env.NODE_ENV === 'development') {
-      console.log(...messages)
+      const caller = new Error().stack.split('\n')[2].trimEnd()
+      console.log(`[${caller}]`, ...messages)
     }
   }
 
   const errorLog = (...messages) => {
     if (process.env.NODE_ENV === 'development') {
+      const caller = new Error().stack.split('\n')[2].trimEnd()
       console.error(...messages)
     }
   }
