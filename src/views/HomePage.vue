d5d4ecd1b6c72aa4828bbd0576baf409aba141b6
rollingn27 <arollingstone27@gmail.com>
2024-02-19 21:43:51 +0900

    feat: 메뉴 호버시 툴팁 생성

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,21 +1,37 @@
 <template>
   <code-harbor-header />
+  <v-dialog v-model="dialog.status" width="35rem">
+    <v-card>
+      <v-card-text> {{ dialog.text }}</v-card-text>
+      <v-card-actions class="justify-end">
+        <v-btn color="info" @click="closeDialog('ok')" v-if="dialog.buttonText1">{{
+          dialog.buttonText1
+        }}</v-btn>
+        <v-btn color="info" @click="closeDialog" ref="logoutCancel">{{ dialog.buttonText2 }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
   <div class="code-harbor-main">
     <div class="side-bar">
       <div class="img-box">
         <img src="@/assets/sidebarIcon/shareproblem.png" alt="문제 공유" />
+        <v-tooltip activator="parent" location="end">문제공유 페이지</v-tooltip>
       </div>
       <div class="img-box">
         <img src="@/assets/sidebarIcon/statistics.png" alt="통계" />
+        <v-tooltip activator="parent" location="end">통계 페이지</v-tooltip>
       </div>
       <div class="img-box">
         <img src="@/assets/sidebarIcon/knowhow.png" alt="노하우" />
+        <v-tooltip activator="parent" location="end">알고리즘 노하우 공유</v-tooltip>
       </div>
       <div class="img-box">
         <img class="cursor-help" src="@/assets/sidebarIcon/help.png" alt="help" />
+        <v-tooltip activator="parent" location="end">Code Harbor 알아보기</v-tooltip>
       </div>
-      <div class="img-box" @click="signOut">
+      <div class="img-box" @click="onSignOut">
         <img src="@/assets/sidebarIcon/logout.png" alt="log out" />
+        <v-tooltip activator="parent" location="end">로그아웃</v-tooltip>
       </div>
     </div>
     <router-view></router-view>
@@ -23,11 +39,32 @@
 </template>
 <script setup>
 import { loginService } from '@/api'
+import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
+const dialog = reactive({
+  status: false,
+  text: '',
+  buttonText1: '',
+  buttonText2: ''
+})
 const signOut = () => {
   loginService.signOut()
   router.push({ path: '/signIn' })
+}
+
+const closeDialog = (type) => {
+  if (type === 'ok') {
+    signOut()
+  }
+  dialog.status = false
+}
+
+const onSignOut = () => {
+  dialog.status = true
+  dialog.text = '로그아웃 하시겠습니까?'
+  dialog.buttonText1 = 'ok'
+  dialog.buttonText2 = 'cancel'
 }
 </script>
 <style lang="scss" scoped>
