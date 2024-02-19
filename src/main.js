c575399e16af1a80d35b06d17ae017578a9018ae
rollingn27 <arollingstone27@gmail.com>
2024-02-19 20:39:32 +0900

    etc: vuetify 적용

--- src/main.js
+++ src/main.js
@@ -8,13 +8,23 @@ import router from './router'
 import LoadingBar from '@/components/common/LoadingBar.vue'
 import LoadingSpinner from '@/components/common/LoadingSpinner.vue'
 import CodeHarborHeader from '@/components/common/CodeHarborHeader.vue'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
-
+const vuetify = createVuetify({
+  components,
+  directives,
+  iconfont: 'mdi'
+})
 app.use(createPinia())
 app.use(router)
 app.component('loading-bar', LoadingBar)
 app.component('loading-spinner', LoadingSpinner)
 app.component('code-harbor-header', CodeHarborHeader)
-
+app.use(vuetify)
 app.mount('#app')
